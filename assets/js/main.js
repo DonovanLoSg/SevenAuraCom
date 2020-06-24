1ded0b4c7754e2b41bbdfc11ababa4a9dce51b9b
Donovan Lo <donovanlo1174@gmail.com>
2020-06-24 16:20:03 +0000

    Add owl carousel to homepage displaying testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,52 @@
+// $('.owl-carousel').owlCarousel({
+//     stagePadding: 14,
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:2
+//         },
+//         1000:{
+//             items:3
+//         }
+//     }
+// })
+
 $('.owl-carousel').owlCarousel({
+    center: true,
+    items: 14,
     loop:true,
     margin:10,
+autoplay:true,
+autoplayTimeout:2000,
+autoplayHoverPause:true,
+autoHeight:true,
+autoWidth:true,
     nav:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:2
         },
         1000:{
-            items:5
+            items:3
         }
     }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
 })
 
+
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel();
+// });
